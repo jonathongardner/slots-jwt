082f9ac08e3c6aceb151363940de66f4edf05f00
Jonathon Gardner <TheAppGardner@gmail.com>
2019-01-10 15:45:53 -0500

    Fixed route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Slots::Engine.routes.draw do
   patch user_model.pluralize, to: 'manages#update'
 
   get 'sign_in', to: 'sessions#sign_in'
-  post 'sign_in'
+  post 'sign_in', to: 'sessions#sign_in'
   delete 'sign_out', to: 'sessions#sign_out'
   get 'update_session_token', to: 'sessions#update_session_token'
   # get 'valid_token', to: 'sessions#valid_token' TODO not sure if valid token is needed
